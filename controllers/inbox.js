d45eb488106277781bab0b22c079ef64f46dc06a
Biruk Habte <biruk.habte-ug@aau.edu.et>
2025-01-04 22:51:48 +0300

    Add patient feedback system

---
+++ controllers/inbox.js
@@ -0,0 +1,31 @@
+var express = require ('express');
+var router = express.Router();
+var db = require.main.require ('./models/supabase_controller');
+var bodyPaser = require ('body-parser');
+router.get('*', function(req, res, next){
+	if(req.cookies['username'] == null){
+		res.redirect('/login');
+	}else{
+		next();
+	}
+});
+
+router.get('/',function(req,res){
+    db.getcomplain(function(err,result){
+        res.render('inbox.ejs',{list :result});
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports =router;
+
+// Updated: 2026-03-27 08:54:45
