98837c15fbd8504de73f6f41a4e7fcfcfc7b6e30
Biruk Habte <biruk.habte-ug@aau.edu.et>
2025-01-11 15:22:50 +0300

    Add patient lab results tracking

---
+++ controllers/chat.js
@@ -0,0 +1,20 @@
+var mysql =require('mysql');
+var express = require ('express');
+var cookie = require ('cookie-parser');
+var router = express.Router();
+var app = express();
+var io = require("socket.io");
+
+
+router.get('/',function(req,res){
+    res.render('chat.ejs');
+});
+
+
+
+
+
+module.exports = router;
+
+
+// Updated: 2026-03-27 08:54:45
